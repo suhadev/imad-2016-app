9a226ffa87e54bac6c7c79258630d18ec1801744
suhadev <suhadevvenkatesh@gmail.com>
2016-09-30 00:36:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var image=document.getElementById("img");
 var marginLeft=0;
 function moveRight(){
     
-    marginLeft= marginLeft + 10;
+    marginLeft= marginLeft + 5;
     image.style.marginLeft= marginLeft + 'px';
 }
 image.onclick=function(){
    
-   var interval=setInterval(moveRight,100);
+   setInterval(moveRight,50);
 };
