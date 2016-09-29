928c8bcaff4d7c15dba20e6ce1a9b89dae256612
suhadev <suhadevvenkatesh@gmail.com>
2016-09-30 00:06:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,8 +99,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/:articleName',function(req,res){
