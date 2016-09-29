e9a4f37a25e1340066c3b12e2284f4919f471dbc
suhadev <suhadevvenkatesh@gmail.com>
2016-09-30 00:28:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
 var image=document.getElementById("img");
+var marginLeft=0;
 function moveRight(){
-    var marginLeft=0;
+    
     marginLeft=marginLeft+10;
     img.style.marginleft=marginLeft+'px';
 }
