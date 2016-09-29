502a092d9cf60b85ab154dd6b0b1c52cb2a45136
suhadev <suhadevvenkatesh@gmail.com>
2016-09-29 16:36:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get('/article-one',function(req,res){
 app.get('/article-two',function(req,res){
    res.send("Article two is required"); 
 });
+app.get('/article-three',function(req,res){
+   res.send("Article three is required"); 
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
