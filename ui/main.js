34e44e7ab2865e2b9330e83eb59963be9608599c
suhadev <suhadevvenkatesh@gmail.com>
2016-09-30 12:20:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,9 @@ image.onclick=function(){
    setInterval(moveRight,50);
 };*/
 var counter=0;
-var button=document.getElementById('count');
+var button1=document.getElementById('count');
 console.log("button identified");
-button.onclick = function(){
+button1.onclick = function(){
     console.log("button clicked");
     counter = counter + 1;
     var span=document.getElementById('counter');
