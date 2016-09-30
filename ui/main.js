5c22f7297a1c5eef7d148d863aa20f4397d70528
suhadev <suhadevvenkatesh@gmail.com>
2016-09-30 12:22:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,11 +11,12 @@ image.onclick=function(){
    setInterval(moveRight,50);
 };*/
 var counter=0;
-var button1=document.getElementById('count');
+var button=document.getElementById('count');
 console.log("button identified");
-button1.onclick = function(){
+button.onclick= alert("hello");
+/*button.onclick = function(){
     console.log("button clicked");
     counter = counter + 1;
     var span=document.getElementById('counter');
     span.innerHTML=counter.toString();
-};
+};*/
