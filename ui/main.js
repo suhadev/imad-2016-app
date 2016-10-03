8ba474dc92c1f0c45bb807be1b77402f25e42c94
suhadev <suhadevvenkatesh@gmail.com>
2016-10-03 09:01:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,16 @@ button.onclick = alert("hello");*/
     var span=document.getElementById('counter');
     span.innerHTML=counter.toString();
 };*/
+var nameInput=document.getElementById("input");
+var nameEntered=nameInput.value;
+var submit=document.getElementById("submitbtn");
+submit.onclick=function(){
+    var names=["name1","name2","name3"];
+    var list="";
+    for(var i=0;i<=names.length;i++){
+        list="<li>"+names[i]+"</li>";
+    }
+    
+};
+var unorderedList=document.getElementById("ul");
+unorderedlist.innerHTML=list;
